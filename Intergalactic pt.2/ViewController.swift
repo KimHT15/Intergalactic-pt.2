3a895aae0b0c2a1515237ca65c05694c7d0944b0
Kimberly Herrera <kimherrera601@gmail.com>
2021-10-20 10:13:19 -0600

    Initial Commit

---
+++ Intergalactic pt.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intergalactic pt.2
+//
+//  Created by Kimberly Herrera on 10/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
